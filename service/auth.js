039d29ddd1d3f7ed43f8b474b4f5fc68d4045772
Siddhi2102 <agrawalsiddhi2102@gmail.com>
2024-08-16 09:25:08 +0530

    statless

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,25 @@
-const sessionIdToUserMap=new Map();
+const jwt = require("jsonwebtoken");
+const secret="Sid123@123";
 
-function setUser(id,user){
-    sessionIdToUserMap.set(id,user);
+function setUser(user){
+
+    return jwt.sign(
+        {
+            _id:user._id,
+            email:user.email,
+        },
+        secret
+    );  
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
+function getUser(token){
+    if(!token)return null;
+    try{
+        return jwt.verify(token,secret);   //vrifying the secret kry if it the one we generated or not
+    } catch(error){
+        return null;
+    }
+    
 }
 
 module.exports={
